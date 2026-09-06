c19f507b387061dec41b92c8c759e28b54c6a53d
asashkov <sashkov.av@gmail.com>
2026-09-06 17:24:53 +0300

    Console bot app has been created as first homework.

---
+++ ConsoleAppBot/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace ConsoleBot;
+
+public class Program
+{
+    private const string StartEndpoint = "/start";
+    private const string HelpEndpoint = "/help";
+    private const string InfoEndpoint = "/info";
+    private const string EchoEndpoint = "/echo";
+    private const string ExitEndpoint = "/exit";
+
+    static void Main()
+    {
+        DateOnly creationDate = DateOnly.Parse("05.09.2026");
+
+        Console.WriteLine($"Hello!\n" +
+            $"Available commands to interact with the console bot: " +
+            $"{StartEndpoint}, {HelpEndpoint}, {InfoEndpoint}, {EchoEndpoint}, {ExitEndpoint}");
+
+        string? userName = null;
+        bool isNameTaken = false;
+
+        while (true)
+        {
+            PrintMenu(isNameTaken, userName);
+
+            var input = Console.ReadLine();
+            string[] inputParams = [];
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                var inputWithParams = input.Split(' ');
+                input = inputWithParams[0];
+                inputParams = inputWithParams.Skip(1).ToArray();
+            }
+
+            switch (input)
+            {
+                case StartEndpoint:
+                    if (isNameTaken)
+                    {
+                        Console.WriteLine($"You are logged in as {userName}. Use one of the other commands.");
+                    }
+                    else
+                    {
+                        Console.Write("Enter your name: ");
+
+                        userName = Console.ReadLine();
+
+                        if (!string.IsNullOrEmpty(userName))
+                        {
+                            isNameTaken = true;
+                        }
+                    }
+
+                    break;
+                case HelpEndpoint:
+                    PrintHelpEndpointText();
+                    break;
+                case InfoEndpoint:
+                    Console.WriteLine("ConsoleBot 0.0.1\n" + $"Cteation Date: {creationDate}");
+                    break;
+                case EchoEndpoint:
+                    PrintEchoEndpointText(isNameTaken, inputParams);
+                    break;
+                case ExitEndpoint:
+                    return;
+                default:
+                    Console.WriteLine("You typed an incorrect command, try again.");
+                    break;
+            }
+        }
+    }
+
+    private static void PrintMenu(bool isNameTaken, string userName)
+    {
+        string greating = !isNameTaken ? "Please" : $"{userName}, please";
+
+        Console.WriteLine();
+        Console.Write($"{greating} enter the command: ");
+    }
+
+    private static void PrintEchoEndpointText(bool isNameTaken, string[] inputParams)
+    {
+        if (!isNameTaken)
+        {
+            Console.WriteLine($"You are not logged in. Type your name using {StartEndpoint} command.");
+            return;
+        }
+
+        if (inputParams.Length > 0)
+        {
+            foreach (var param in inputParams)
+            {
+                Console.Write(param + " ");
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    private static void PrintHelpEndpointText()
+    {
+        Console.WriteLine("To interact with the console bot you need to use following commands:\n" +
+            $"{StartEndpoint}\t - Program entry point. Type your name to get more available commands.\n" +
+            $"{HelpEndpoint}\t - Display this info.\n" +
+            $"{InfoEndpoint}\t - Display program version and creation date.\n" +
+            $"{EchoEndpoint}\t - You make to print on screen some text if you type it after command using a space character for separation.\n" +
+            $"{ExitEndpoint}\t - Exit from program.");
+    }
+}
